f9a9732d97da2b59a36396fc63f4fa2d79e31637
vieiramanda11 <amanda_vieira@outlook.com>
2019-10-10 19:16:29 -0300

    fixing production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = sample-app-book-tutorial.herokuapp.com'
+  host = 'sample-app-book-tutorial.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
